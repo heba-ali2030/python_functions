3cd00d0e27c20de32429a8825e8e17e25494dcf2
heba-ali2030 <heba.galaa@gmail.com>
2021-06-06 23:05:57 +0200

    funtion arguments are finished

---
+++ funtion_arguements.py
@@ -0,0 +1,34 @@
+## Python Keyword Arguments
+
+#  def greet(name, message= 'How do you do ?!'):
+#     ''' user defined function to greet someone with modifiable message'''
+#     print(f'hello {name}, {message}')
+
+# greet('Heba', 'Good Morning ^_^')
+# greet('Hossam')
+
+
+# def greet(message, name):
+#     ''' user defined function to greet someone with modifiable message'''
+#     print(f'hello {name}, {message}')
+
+# greet('Heba', 'Good Morning ^_^')
+
+# def greet(name, message='hi'):
+#     print(f'hello {name}, {message}')
+# greet('morning', 'heba')
+
+
+# #Python Arbitrary Arguments
+
+def greet(*names):
+     """This function greets all the person in the names tuple."""
+    
+     for name in names:
+      print(f'hello {name} , How are you ?!')
+
+greet('heba', 'hala' , 'ahmed')
+
+
+
+
